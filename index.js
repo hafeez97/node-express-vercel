ef990fad65e4794d09b25720dd38e06e5d2205df
Hafeez <hafeezshaikh197@gmail.com>
2023-04-01 09:36:43 +0500

    get post

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
+// const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+// app.use("/home", home);
+
+app.get('/', (req, res) => {
+    res.send('Webhook App is running!');
+});
+
+app.post('/webhook', (req, res) => {
+    console.log('Received a webhook request:', req.body);
+    // handle the webhook request here
+    console.log(req.body);
+    res.sendStatus(200);
+});
 
 // connection
 const port = process.env.PORT || 9001;
